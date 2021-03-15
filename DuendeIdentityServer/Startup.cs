730f7066e092944c69024c7eddcd39243fe7102c
Mohamed Yousuf <mohamed.yousuf@outlook.com>
2021-03-16 00:46:30 +0300

    update

--- DuendeIdentityServer/Startup.cs
+++ DuendeIdentityServer/Startup.cs
@@ -50,8 +50,8 @@ namespace DuendeIdP
                 .AddInMemoryClients(new List<Client> { RelyingParty })
                 .AddWsFederationPlugin(options =>
                 {
-                    options.Licensee = "";
-                    options.LicenseKey = "";
+                    options.Licensee = "DEMO";
+                    options.LicenseKey = "eyJTb2xkRm9yIjowLjAsIktleVByZXNldCI6NiwiU2F2ZUtleSI6ZmFsc2UsIkxlZ2FjeUtleSI6ZmFsc2UsIlJlbmV3YWxTZW50VGltZSI6IjAwMDEtMDEtMDFUMDA6MDA6MDAiLCJhdXRoIjoiREVNTyIsImV4cCI6IjIwMjEtMDQtMTFUMDA6MDA6MDMuOTcwNzIyOSswMTowMCIsImlhdCI6IjIwMjEtMDMtMTJUMDA6MDA6MDMiLCJvcmciOiJERU1PIiwiYXVkIjozfQ==.Q2ilORcBkIwwAMCcDkkc4JsRKaCJnFkOkxycgEhK9XKu/D+jHjrHiWUHz302BIPVzvS5JHL2FJHQ7QtF3zcgoo5lVzm7VQVfl5I6GHjx1QCUdhVAFEwtJu3005gQlJ1iCMjandIljSbo7hFnffpS06B1aQTD8TGojF1hJP3xLfzBrnIZ4L5CRWk3gWQBrESpJ2H6TH4j2iSZAZ2NvFgjtbcebjcrSZfmUjhNcMdUrmgpQDPpPZm4KRU1BlWHW8BSyW9XIq+HpyMyifKI4Jkf+CWqI2/AWo+R/sufKnKfJWlXd2gCp9gUUi5BRV1JBTOvkDyvpCnRHCSuITB8Tn5zOC5KuzCg5Nbeg/hELMIS8VcC3B6Qx+gUfePESnNsn5C7xsLRRfDu6FaCVt/F0QWhi8JtSg1rgWvEJyYqnt+78bcDZ4lLva3xKTK66N3+bgYaujF2enp5bgOGGvRT7j/ZSFnt8UbDYgXRfIQNTj4XXlTparehEZ0n7s1EqJBy2H0EBPXLYcruj1bz3UmAdsqj+cS2W03ERWm3qIPiR40oirXT+RKTdp0cfRLSaVzhUE0RUypmscNF7NlLuMiIKqM3iLZLGryUHQRSlpJ0GnDBYHvdleXhgHyK9zaoBu53ivtsmkkDEkaLaNHEfA9U+IibL2Elj+JgSV23u2dAKC4N0N4=";
                 })
                 .AddInMemoryRelyingParties(new List<RelyingParty>());
         }
